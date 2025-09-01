eb5abc29b2f8065f95686c05a8269fe7e6889f74
Shaikh Ubaid Ahmed <55990230+shaikhubaidahmed@users.noreply.github.com>
2025-09-01 22:19:41 +0530

    Time: 18 ms (60.80%) | Memory: 43.9 MB (62.51%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -1,21 +1,22 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] arr, int target) {
-        int n=arr.length;
-        List<List<Integer>> list=new ArrayList<List<Integer>>();
-        if(n<=3){
-            return list;    
-        }
+        int n = arr.length;
+        List<List<Integer>> list = new ArrayList<>();
         Arrays.sort(arr);
-        for(int i=0;i<n-3;i++){
-            if(i!=0 && arr[i]==arr[i-1]) continue;
-            for(int j=i+1;j<n-2;j++){
-                if(j!=i+1 && arr[j]==arr[j-1]) continue;
-                int low=j+1;
-                int high=n-1;
-                long subt=(long)target-arr[i]-arr[j];
-                while(low<high){
-                    if(arr[low]+arr[high]==subt){
-                        List<Integer> sublist=new ArrayList<>();
+        for (int i = 0; i <= n -1; i++) {
+            if (i > 0 && arr[i] == arr[i - 1])
+                continue;
+            for (int j = i + 1; j <= n - 1; j++) {
+                if (j !=i+1 && arr[j] == arr[j - 1])
+                    continue;
+                int low = j + 1;
+                int high = n - 1;
+
+                
+                while (low < high) {
+                    long sum=(long)arr[i]+arr[j]+arr[low]+arr[high];
+                    if (sum == target) {
+                        List<Integer> sublist = new ArrayList<>();
                         sublist.add(arr[i]);
                         sublist.add(arr[j]);
                         sublist.add(arr[low]);
@@ -23,27 +24,19 @@ class Solution {
                         list.add(sublist);
                         low++;
                         high--;
-                        while(low<high && arr[low]==arr[low-1]){
+
+                        while (low<high && arr[low] == arr[low - 1])
                             low++;
-                        }
-                        while(low<high && arr[high]==arr[high+1]){
+                        while (low<high && arr[high] == arr[high + 1])
                             high--;
-                        }
-                        
-                    }else if(arr[low]+arr[high]<subt){
-                        low++;
-                    }else{
+                    } else if (sum > target) {
                         high--;
+                    } else {
+                        low++;
                     }
                 }
-                // while(j+1<n-2 && arr[j]==arr[j+1]){
-                //     j++;
-                // }
-            }//j loop
-            // while(i+1<n-3 && arr[i]==arr[i+1]){
-            //     i++;
-            // }
+            }
         }
-        return  list;
+        return list;
     }
 }
