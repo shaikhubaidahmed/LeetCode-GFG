cbccb38d89e40ecee4e95fb96111c2b952ab93d4
Shaikh Ubaid Ahmed <55990230+shaikhubaidahmed@users.noreply.github.com>
2025-10-09 19:39:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Number of occurrence/number-of-occurrence.java
@@ -0,0 +1,47 @@
+class Solution {
+    int countFreq(int[] arr, int k) {
+        // code here
+        
+        int first=-1;
+        int last=-1;
+        int n=arr.length;
+        int low=0;
+        int high=n-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            
+            if(arr[mid]<k){
+                low=mid+1;
+            }else if(arr[mid]>k){
+                high=mid-1;
+            }else{
+                first=mid;
+                high=mid-1;
+                
+            }
+        }
+
+        if (first==-1) return 0;
+
+        //  int n=arr.length;
+        low=0;
+        high=n-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            
+            if(arr[mid]<k){
+                low=mid+1;
+            }else if(arr[mid]>k){
+                high=mid-1;
+            }else{
+                last=mid;
+                low=mid+1;
+                
+                
+            }
+        }
+        // return low;
+
+        return last-first+1;
+    }
+}
