65450b76ec6934d2d59aafc571f97353ab92c677
Shaikh Ubaid Ahmed <55990230+shaikhubaidahmed@users.noreply.github.com>
2026-09-23 02:23:57 +0530

    Sync LeetCode - 122. Best Time to Buy and Sell Stock II (Medium)

---
+++ LeetCode/Medium/122_BestTimeToBuyAndSellStockII.java
@@ -0,0 +1,27 @@
+/**
+ * Problem Link : https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+ * Platform     : LeetCode
+ * Difficulty   : Medium
+ */
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n=prices.length;
+        int[][] dp=new int[n+1][2];
+        dp[n][0]=0;
+        dp[n][1]=0;
+
+        for(int ind=n-1;ind>=0;ind--){
+            for(int i=0;i<=1;i++){
+                int profit=0;
+                if(i==1){
+                    profit=Math.max(-prices[ind]+dp[ind+1][0],dp[ind+1][1]);
+                }else{
+                    profit=Math.max(prices[ind]+dp[ind+1][1],dp[ind+1][0]);
+                }
+                dp[ind][i]=profit;
+            }
+        }
+        return dp[0][1];
+    }
+}
